26505ffe1080f8421427f1d6337a2c7a8f25920e
Karine Brandelli <karinebrandelli@live.com>
2023-03-03 05:59:45 -0300

    build: implement Tootltip for copy button

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import copy from 'copy-to-clipboard'
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
 
 export const Card = () => {
   const [passLength, setPassLength] = useState<number | string>(6);
@@ -66,6 +68,11 @@ export const Card = () => {
             <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
           </svg>
         </button>
+        <Tooltip
+          id="copy-button"
+          place="right"
+          className="font-semibold"
+          style={{ backgroundColor: "#075985", color: "#FFFFFF" }} />
       </div>
 
       <div className="text-base font-semibold sm:text-lg" >
